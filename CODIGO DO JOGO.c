c0d37548078c2cb6e43fba81d2c652abf1b47ba8
wisley905 <owisley353@gmail.com>
2025-03-31 20:07:04 -0300

    Update CODIGO DO JOGO.c

--- CODIGO DO JOGO.c
+++ CODIGO DO JOGO.c
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <stdio.h>//Para exibir perguntas e ler respostas
 #include <stdlib.h>//Para limpar a tela (system("cls")) e alocar memória
